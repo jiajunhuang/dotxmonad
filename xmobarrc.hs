897520e262139ad44a1040017750d364c1214a50
Jiajun Huang <gansteed@gmail.com>
2016-12-18 16:54:13 +0800

    format xmonad config

--- xmobarrc.hs
+++ xmobarrc.hs
@@ -1,25 +1,29 @@
 Config {
-        font = "xft:WenQuanYi Zen Hei:size=10"
-      , bgColor = "#353945"
-      , fgColor = "#ffffff"
-      , position = TopW L 90
-      , sepChar = "%" -- delineator between plugin names and straight text
-      , alignSep = "}{" -- separator between left-right alignment
-      , template = "%StdinReader% }{ %memory% | %battery% | %date%          "
-      , commands = [
-      Run StdinReader -- read data from xmonad
-      , Run Memory [ "--template" ,"Mem: <usedratio>%"
-      , "--Low", "20"
-      , "--High", "90"
-      , "--low", "green"
-      , "--normal", "orange"
-      , "--high", "red" ] 60
-      , Run Battery [ "--template" , "Bat: <left>%"
-      , "--Low", "30"
-      , "--High", "90"
-      , "--low" , "red"
-      , "--normal" , "orange"
-      , "--high" , "green"] 60
-      , Run Com "date" ["+%a %b %_d %H:%M"] "date" 60
+    font = "xft:WenQuanYi Zen Hei:size=10",
+    bgColor = "#353945",
+    fgColor = "#ffffff",
+    position = TopW L 90,
+    sepChar = "%", -- delineator between plugin names and straight text
+    alignSep = "}{", -- separator between left-right alignment
+    template = "%StdinReader% }{ %memory% | %battery% | %date%          ",
+    commands = [
+        Run StdinReader, -- read data from xmonad
+        Run Memory [
+            "--template" ,"Mem: <usedratio>%",
+            "--Low", "20",
+            "--High", "90",
+            "--low", "green",
+            "--normal", "orange",
+            "--high", "red"
+        ] 60,
+        Run Battery [
+            "--template" , "Bat: <left>%",
+            "--Low", "30",
+            "--High", "90",
+            "--low" , "red",
+            "--normal" , "orange",
+            "--high" , "green"
+        ] 60,
+        Run Com "date" ["+%a %b %_d %H:%M"] "date" 60
     ]
 }
--- xmonad.hs
+++ xmonad.hs
@@ -1,32 +1,29 @@
+import Graphics.X11.ExtraTypes.XF86
+import System.Exit
+import System.IO
 import XMonad
+import XMonad.Actions.GridSelect
+import XMonad.Actions.SpawnOn
 import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.EwmhDesktops
+import XMonad.Hooks.ICCCMFocus
 import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.ManageHelpers
-import XMonad.Hooks.ICCCMFocus
-import XMonad.Hooks.EwmhDesktops
 import XMonad.Layout.NoBorders
-import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
-import XMonad.Actions.SpawnOn
-import XMonad.Actions.GridSelect
-import Graphics.X11.ExtraTypes.XF86
-import System.IO
-import System.Exit
-import qualified XMonad.StackSet as W
+import XMonad.Util.Run(spawnPipe)
 import qualified Data.Map as M
+import qualified XMonad.StackSet as W
 
 --ManageHooks
 myManageHook = composeAll [
-    isFullscreen --> (doF W.focusDown <+> doFullFloat)
-    , isDialog --> doFloat
-    , appName =? "netease-cloud-music" --> doShift "6-music"
-    , appName =? "xchat" --> doShift "7-chat"
-    , appName =? "VirtualBox" --> doShift "8-vm"
-    , appName =? "desktop_window" --> doIgnore
+    isFullscreen --> (doF W.focusDown <+> doFullFloat),
+    isDialog --> doFloat,
+    appName =? "desktop_window" --> doIgnore
     ]
 
 -- Define the names of all workspaces
-myWorkspaces = ["1-docs", "2-code", "3-code", "4-code", "5-mail", "6-music", "7-chat", "8-vm", "9-others"]
+myWorkspaces = ["1-docs", "2-code", "3-code", "4-mail", "5-chat", "6", "7", "8", "9"]
 
 -- Define Terminal
 myTerminal = "gnome-terminal"
@@ -46,29 +43,28 @@ main = do
     spawn "feh --bg-scale ~/.xmonad/background.jpg"
     xmproc <- spawnPipe "xmobar ~/.xmonad/xmobarrc.hs"
     xmonad $ ewmh defaultConfig {
-        manageHook = myManageHook <+> manageDocks <+> manageHook defaultConfig
-        , handleEventHook = docksEventHook <+> handleEventHook defaultConfig
-        , layoutHook = avoidStruts $ smartBorders $ myLayout
-        , terminal = myTerminal
-        , logHook = do
+        manageHook = myManageHook <+> manageDocks <+> manageHook defaultConfig,
+        handleEventHook = docksEventHook <+> handleEventHook defaultConfig,
+        layoutHook = avoidStruts $ smartBorders $ myLayout,
+        terminal = myTerminal,
+        logHook = do
             takeTopFocus
             dynamicLogWithPP $ xmobarPP {
-                ppOutput = hPutStrLn xmproc
-                , ppTitle = xmobarColor "#fdf6e3" "" . shorten 60
-                , ppLayout = const "" -- to disable the layout info on xmobar
-            }
-            , borderWidth = 1
-            , workspaces = myWorkspaces
-            , normalBorderColor = myNormalBorderColor
-            , focusedBorderColor = myFocusedBorderColor
+                ppOutput = hPutStrLn xmproc,
+                ppTitle = xmobarColor "#fdf6e3" "" . shorten 60,
+                ppLayout = const "" -- to disable the layout info on xmobar
+            },
+            borderWidth = 1,
+            workspaces = myWorkspaces,
+            normalBorderColor = myNormalBorderColor,
+            focusedBorderColor = myFocusedBorderColor
             } `additionalKeys` [
-                ((mod4Mask, xK_l), spawn "slock")
-                , ((0, xF86XK_MonBrightnessUp), spawn "xbacklight +20")
-                , ((0, xF86XK_MonBrightnessDown), spawn "xbacklight -20")
-                , ((mod4Mask, xK_c), spawn "chromium")
-                , ((mod4Mask, xK_e), spawn "zathura")
-                , ((mod4Mask, xK_m), spawn "netease-cloud-music")
-                , ((mod4Mask, xK_t), spawn "touchpad_toggle.sh")
-                , ((mod4Mask, xK_b), sendMessage ToggleStruts)
-                , ((mod4Mask, xK_Tab), goToSelected defaultGSConfig)
+                ((mod4Mask, xK_l), spawn "slock"),
+                ((0, xF86XK_MonBrightnessUp), spawn "xbacklight +20"),
+                ((0, xF86XK_MonBrightnessDown), spawn "xbacklight -20"),
+                ((mod4Mask, xK_c), spawn "chromium"),
+                ((mod4Mask, xK_e), spawn "zathura"),
+                ((mod4Mask, xK_t), spawn "touchpad_toggle.sh"),
+                ((mod4Mask, xK_b), sendMessage ToggleStruts),
+                ((mod4Mask, xK_Tab), goToSelected defaultGSConfig)
                 ]
