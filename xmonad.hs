4947ad9249d8997f65ba53dda86b2f890a8ea148
Jiajun Huang <gansteed@gmail.com>
2017-02-14 11:35:39 +0800

    update XMonad config

--- xmonad.hs
+++ xmonad.hs
@@ -2,23 +2,25 @@ import Graphics.X11.ExtraTypes.XF86
 import System.Exit
 import System.IO
 import XMonad
+import XMonad.Config.Desktop
 import XMonad.Actions.GridSelect
 import XMonad.Actions.SpawnOn
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.EwmhDesktops
-import XMonad.Hooks.ICCCMFocus
 import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.ManageHelpers
 import XMonad.Layout.NoBorders
-import XMonad.Util.EZConfig(additionalKeys)
-import XMonad.Util.Run(spawnPipe)
+import XMonad.Prompt
+import XMonad.Prompt.Shell
+import XMonad.Util.EZConfig (additionalKeys)
+import XMonad.Util.Run (spawnPipe)
 import qualified Data.Map as M
 import qualified XMonad.StackSet as W
 
 --ManageHooks
 myManageHook = composeAll [
     isFullscreen --> (doF W.focusDown <+> doFullFloat),
-    isDialog --> doFloat,
+    isDialog --> doCenterFloat,
     appName =? "desktop_window" --> doIgnore
     ]
 
@@ -40,34 +42,40 @@ myNormalBorderColor = "#353945"
 myFocusedBorderColor = "#ffffff"
 myXmobarColorfg = "#fdf6e3"
 myXmobarColorbg = ""
+myXmonadPromptConfig = def {
+    position = Top,
+    alwaysHighlight = True,
+    promptBorderWidth = 0,
+    font = "xft:monospace:size=12"
+}
+
 
 main = do
     spawn "feh --bg-scale ~/.xmonad/background.jpg"
     xmproc <- spawnPipe "xmobar ~/.xmonad/xmobarrc.hs"
-    xmonad $ ewmh defaultConfig {
-        manageHook = myManageHook <+> manageDocks <+> manageHook defaultConfig,
-        handleEventHook = docksEventHook <+> handleEventHook defaultConfig,
-        layoutHook = avoidStruts $ smartBorders $ myLayout,
+    xmonad $ desktopConfig {
+        manageHook = myManageHook <+> manageDocks <+> manageHook desktopConfig,
+        handleEventHook = docksEventHook <+> handleEventHook desktopConfig,
+        layoutHook = avoidStruts $ smartBorders myLayout,
         terminal = myTerminal,
-        logHook = do
-            takeTopFocus
-            dynamicLogWithPP $ xmobarPP {
+        logHook = dynamicLogWithPP $ xmobarPP {
                 ppOutput = hPutStrLn xmproc,
                 ppTitle = xmobarColor myXmobarColorfg myXmobarColorbg . shorten 60,
                 ppLayout = const "" -- to disable the layout info on xmobar
             },
-            borderWidth = 1,
-            workspaces = myWorkspaces,
-            normalBorderColor = myNormalBorderColor,
-            focusedBorderColor = myFocusedBorderColor
-            } `additionalKeys` [
-                ((mod4Mask, xK_l), spawn "slock"),
-                ((0, xF86XK_MonBrightnessUp), spawn "xbacklight +20"),
-                ((0, xF86XK_MonBrightnessDown), spawn "xbacklight -20"),
-                ((mod4Mask, xK_c), spawn "chromium"),
-                ((mod4Mask, xK_e), spawn "zathura"),
-                ((mod4Mask, xK_a), spawn "gnome-screenshot --interactive"),
-                ((mod4Mask, xK_t), spawn "touchpad_toggle.sh"),
-                ((mod4Mask, xK_b), sendMessage ToggleStruts),
-                ((mod4Mask, xK_Tab), goToSelected defaultGSConfig)
-                ]
+        borderWidth = 1,
+        workspaces = myWorkspaces,
+        normalBorderColor = myNormalBorderColor,
+        focusedBorderColor = myFocusedBorderColor
+    } `additionalKeys` [
+            ((mod4Mask, xK_l), spawn "slock"),
+            ((0, xF86XK_MonBrightnessUp), spawn "xbacklight +20"),
+            ((0, xF86XK_MonBrightnessDown), spawn "xbacklight -20"),
+            ((mod4Mask, xK_c), spawn "chromium"),
+            ((mod4Mask, xK_e), spawn "zathura"),
+            ((mod4Mask, xK_a), spawn "gnome-screenshot --interactive"),
+            ((mod4Mask, xK_t), spawn "touchpad_toggle.sh"),
+            ((mod4Mask, xK_b), sendMessage ToggleStruts),
+            ((mod1Mask, xK_Tab), goToSelected def),
+            ((mod1Mask, xK_p), shellPrompt myXmonadPromptConfig)
+        ]
