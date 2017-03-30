a7f2cf3a141f30f3e2ad3eea1018f50b26464572
Jiajun Huang <gansteed@gmail.com>
2017-03-30 23:50:21 +0800

    use byobu

--- bash/bash_profile.sh
+++ bash/bash_profile.sh
@@ -4,3 +4,6 @@
 
 [[ -f ~/.xmonad/bash/bashrc.sh ]] && . ~/.xmonad/bash/bashrc.sh
 [[ -f ~/.bashrc ]] && . ~/.bashrc
+if [ -x `command -v byobu` ]; then
+    _byobu_sourced=1 . /usr/bin/byobu-launch 2>/dev/null || true
+fi
