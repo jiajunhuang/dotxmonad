6e32c5ffa114a38ec6159035c625dd918dd49d0a
Jiajun Huang <gansteed@gmail.com>
2017-04-12 15:28:14 +0800

    add programs

--- xmonad.hs
+++ xmonad.hs
@@ -28,7 +28,7 @@ myManageHook = composeAll [
     isDialog --> doCenterFloat,
     appName =? "desktop_window" --> doIgnore,
     className =? "panel" --> doIgnore, -- panel/trayer
-    className =? "Pidgin" --> doShift "6-chat",
+    className =? "bearychat_v2" --> doShift "6-chat",
     className =? "mysql-workbench-bin" --> doShift "7:sql",
     className =? "Thunderbird" --> doShift "8:mail"
     ]
@@ -43,6 +43,7 @@ myStartupHook = do
     spawnOnce "udiskie -aN"
     spawnOnce "nm-applet"
     spawnOnce "sogou-qimpanel"
+    spawnOnce "bearychat"
 
 -- Define the names of all workspaces
 myWorkspaces = ["1-docs", "2-code", "3-code", "4-test", "5-test", "6-chat", "7:sql", "8:mail", "9:vm"]
