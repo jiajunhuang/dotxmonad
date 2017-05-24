7c750f722af9ab7f68c4128688465714470b7533
Jiajun Huang <gansteed@gmail.com>
2017-05-24 18:13:12 +0800

    tmux only works with ssh

--- bash/bash_profile.sh
+++ bash/bash_profile.sh
@@ -2,8 +2,4 @@
 # ~/.bash_profile
 #
 
-[[ -f ~/.xmonad/bash/bashrc.sh ]] && . ~/.xmonad/bash/bashrc.sh
 [[ -f ~/.bashrc ]] && . ~/.bashrc
-if [ -x `command -v byobu` ]; then
-    _byobu_sourced=1 . /usr/bin/byobu-launch 2>/dev/null || true
-fi
