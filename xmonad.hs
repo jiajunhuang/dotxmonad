28c3eb649014dd0591c358028c57c7bd82053837
Jiajun Huang <gansteed@gmail.com>
2017-06-03 14:18:30 +0800

    add shortcuts

--- xmonad.hs
+++ xmonad.hs
@@ -79,6 +79,8 @@ myShortCuts = [
     ((0, xF86XK_MonBrightnessUp), spawn "xbacklight +20"),
     ((0, xF86XK_MonBrightnessDown), spawn "xbacklight -20"),
     ((mod4Mask, xK_c), spawn "google-chrome"),
+    ((mod4Mask, xK_m), spawn "mysql-workbench"),
+    ((mod4Mask, xK_t), spawn "thunderbird"),
     ((mod4Mask, xK_a), spawn "sleep 0.2; scrot -s -e 'mv $f /data/samba/shots/'"),
     ((mod4Mask, xK_b), sendMessage ToggleStruts),
     ((mod1Mask, xK_Tab), goToSelected def),
