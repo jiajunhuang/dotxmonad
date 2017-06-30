472bf0d268d806b918e3774d50189c048b483a27
Jiajun Huang <gansteed@gmail.com>
2017-06-30 14:14:27 +0800

    add shortcuts for pandoc

--- bash/bashrc.sh
+++ bash/bashrc.sh
@@ -122,4 +122,7 @@ function syncto {
 alias python=python3
 alias bp=bpython
 
+# pandoc
+alias tohtml='pandoc -s -S --toc --highlight-style pygments --css common.css -t html $@ > /data/doc/$@.html'
+
 [ -f ~/.fzf.bash ] && source ~/.fzf.bash
