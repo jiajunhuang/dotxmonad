a1cfa43d58bef2cced8390aaa4dc447a04832ffb
Jiajun Huang <gansteed@gmail.com>
2017-11-07 16:12:59 +0800

    update PATH

--- bash/bashrc.sh
+++ bash/bashrc.sh
@@ -48,7 +48,7 @@ export HISTSIZE=
 
 # golang
 export GOPATH=$HOME/golang
-export PATH=$HOME/.py3k/bin:$GOPATH/bin:/usr/local/openresty/bin:$PATH
+export PATH=$HOME/.py3k/bin:$GOPATH/bin:$HOME/bin:/usr/local/openresty/bin:$PATH
 
 # set default enviroment
 export TERM=xterm-256color
