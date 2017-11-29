c55b2da4ea8e152172cbd23f5ea42cef58854d8b
Jiajun Huang <gansteed@gmail.com>
2017-11-29 16:51:55 +0800

    fix PATH order

--- bash/bashrc.sh
+++ bash/bashrc.sh
@@ -48,7 +48,7 @@ export HISTSIZE=
 
 # golang
 export GOPATH=$HOME/golang
-export PATH=$HOME/.py3k/bin:$GOPATH/bin:$HOME/bin:/usr/local/openresty/bin:$PATH
+export PATH=$PATH:/usr/local/openresty/bin:$HOME/.py3k/bin:$GOPATH/bin:$HOME/bin
 
 # set default enviroment
 export TERM=xterm-256color
