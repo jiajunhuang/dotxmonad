0339ea2a902fc12fadf012c93a7568995d48410f
Jiajun Huang <gansteed@gmail.com>
2018-11-09 14:29:28 +0800

    use ycm extra conf

---
+++ ycm_extra_conf.py
@@ -0,0 +1,23 @@
+flags = [
+    '-Wall',
+    '-Wextra',
+    '-Werror',
+    '-Wno-long-long',
+    '-Wno-variadic-macros',
+    '-fexceptions',
+    '-ferror-limit=10000',
+    '-DNDEBUG',
+    '-std=c99',
+    '-xc',
+    '-isystem/usr/include/',
+]
+
+
+SOURCE_EXTENSIONS = ['.cpp', '.cxx', '.cc', '.c', ]
+
+
+def FlagsForFile(filename, **kwargs):
+    return {
+        'flags': flags,
+        'do_cache': True
+    }
