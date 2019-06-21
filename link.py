fc11148a1e1a831ce90cccaaa866a49ea0d5320b
Jiajun Huang <gansteed@gmail.com>
2019-06-21 11:29:30 +0800

    well, back to YCM

--- link.py
+++ link.py
@@ -7,7 +7,6 @@ MAPPER = {
     "bash/bash_profile.sh": "~/.bash_profile",
     "ack/ackrc": "~/.ackrc",
     "vim/vimrc": "~/.config/nvim/init.vim",
-    "vim/coc-settings.json", "~/.config/nvim/coc-settings.json",
 }
 
 for k, v in MAPPER.items():
