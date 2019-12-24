7b38d513e40648e0e4b810b6e6b8ff45b3d99147
Jiajun Huang <gansteed@gmail.com>
2019-12-24 20:06:28 +0800

    add go/bin to PATH

--- conf/bashrc.sh
+++ conf/bashrc.sh
@@ -62,7 +62,7 @@ export HISTFILESIZE=
 export HISTSIZE=
 
 # PATH
-export PATH="$HOME/.pyenv/bin:$HOME/flutter/bin:$HOME/bin:/snap/bin:$PATH"
+export PATH="$HOME/.pyenv/bin:$HOME/flutter/bin:$HOME/bin:$HOME/go/bin:/snap/bin:$PATH"
 
 # pyenv
 which pyenv 2>&1 > /dev/null
