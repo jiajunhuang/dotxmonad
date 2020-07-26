7ec1b811166003f5627d47c3cb798c3445d104b8
Jiajun Huang <gansteed@gmail.com>
2020-07-26 21:39:43 +0800

    update tmux script so the terminal color works

--- conf/tmux.py
+++ conf/tmux.py
@@ -1,29 +1,31 @@
 #!/usr/bin/env python3
 
 import os
+import copy
 import sys
 import subprocess
 
 
-def get_sessions():
-    sessions = []
+sessions = []
 
-    output = subprocess.Popen(["tmux", "list-sessions"], stdout=subprocess.PIPE).communicate()[0]
-    if sys.stdout.encoding is None:
-        output = output.decode("UTF-8")
-    else:
-        output = output.decode(sys.stdout.encoding)
-    if output:
-        for s in output.splitlines():
-            # Ignore hidden sessions (named sessions that start with a "_")
-            if s and not s.startswith("_"):
-                sessions.append(s.strip())
-    return sessions
+output = subprocess.Popen(["tmux", "list-sessions"], stdout=subprocess.PIPE).communicate()[0]
+if sys.stdout.encoding is None:
+    output = output.decode("UTF-8")
+else:
+    output = output.decode(sys.stdout.encoding)
+if output:
+    for s in output.splitlines():
+        # Ignore hidden sessions (named sessions that start with a "_")
+        if s and not s.startswith("_"):
+            sessions.append(s.strip())
 
 
-sessions = get_sessions()
+env = copy.deepcopy(os.environ)
+env.update({"TERM": "xterm-256color", "HOME": "/home/jiajun"})
+
+
 if sessions:
     session_name = sessions[-1].split(":")[0]
-    os.execvp("tmux", ["tmux", "attach", "-t", session_name])
+    os.execvpe("tmux", ["tmux", "attach", "-t", session_name], env)
 else:
-    os.execvp("tmux", ["tmux", "new"])
+    os.execvpe("tmux", ["tmux", "new"], env)
