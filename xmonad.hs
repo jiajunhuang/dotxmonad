934cf537947a7a11cb1b7742ee6d9952e59b8683
Jiajun Huang <gansteed@gmail.com>
2021-01-23 22:52:17 +0800

    add firefox

--- xmonad.hs
+++ xmonad.hs
@@ -71,6 +71,8 @@ myShortCuts = [
     ((mod4Mask, xK_l), spawn "slock"),
     ((0, xF86XK_MonBrightnessUp), spawn "xbacklight +5"),
     ((0, xF86XK_MonBrightnessDown), spawn "xbacklight -5"),
+    -- browser: firefox
+    ((mod4Mask, xK_f), spawn "firefox"),
     -- browser: chromium
     ((mod4Mask, xK_c), spawn "chromium"),
     -- Mail
