e3ac6fc936d19a88da10bf1fcd3bd8f382bd4a08
Jiajun Huang <gansteed@gmail.com>
2021-07-01 10:10:16 +0800

    add nvm

--- conf/bashrc.sh
+++ conf/bashrc.sh
@@ -201,3 +201,7 @@ function proxyoff() {
 }
 
 [ -f ~/.fzf.bash ] && source ~/.fzf.bash
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
