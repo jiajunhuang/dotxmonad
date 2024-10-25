5dfe7a69d9fcc5e0ee1bc604ef7757bd4261739d
Jiajun Huang <gansteed@gmail.com>
2024-10-25 10:11:06 +0800

    update

--- conf/bash_profile.sh
+++ conf/bash_profile.sh
@@ -3,7 +3,7 @@
 #
 
 # start tmux
-if [[ -z "$TMUX"  ]] && [ "$SSH_CONNECTION" != ""  ]; then
+if [[ -z "$TMUX"  ]] && [ "$SSH_CONNECTION" != "" ] && [ "$TERM_PROGRAM" == "" ]; then
     exec ~/.xmonad/conf/tmux.py
 fi
 
