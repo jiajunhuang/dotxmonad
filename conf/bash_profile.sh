6828cb0390937991f5151548268d00824e366711
Jiajun Huang <gansteed@gmail.com>
2026-01-27 10:05:23 +0800

    byobu on macOS

--- conf/bash_profile.sh
+++ conf/bash_profile.sh
@@ -4,6 +4,12 @@
 
 [[ -f ~/.bashrc ]] && . ~/.bashrc
 
-if command -v byobu > /dev/null; then
-    _byobu_sourced=1 . /usr/bin/byobu-launch 2>/dev/null || true
+if [[ -n "$SSH_CONNECTION" || -n "$SSH_CLIENT" ]]; then
+    if command -v byobu >/dev/null 2>&1; then
+        if [[ "$(uname)" == "Darwin" ]]; then
+            _byobu_sourced=1 . /opt/homebrew/bin/byobu-launch 2>/dev/null || true
+        else
+            _byobu_sourced=1 . /usr/bin/byobu-launch 2>/dev/null || true
+        fi
+    fi
 fi
